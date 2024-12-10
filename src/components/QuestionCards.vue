52593ed7fa468aa8507bf41d7028e83e8565efe9
Expant <anton532202@gmail.com>
2024-12-10 17:01:42 +0300

    add question cards

--- src/components/QuestionCards.vue
+++ src/components/QuestionCards.vue
@@ -1,19 +1,21 @@
-<script setup>  
-import axios from 'axios'
+<script setup>
+import axiosApiInstance from '@/api'
 import QuestionCardsItem from '@/components/QuestionCardsItem.vue'
 import ClipLoader from 'vue-spinner/src/ClipLoader.vue'
 import { useAuthStore } from '@/stores/auth'
 import { ref, onMounted } from 'vue'
 
 const authStore = useAuthStore()
-const cards = ref([]) 
+const cards = ref([])
 const showLoader = ref(false)
+
+// FIXME: fix db rules
+const url = `https://vue-surveys-96197-default-rtdb.europe-west1.firebasedatabase.app/questions`
 
 const getAllCards = async () => {
   showLoader.value = true
   try {
-    const response = await axios.get(`https://vue-surveys-96197-default-rtdb.europe-west1.firebasedatabase.app/questions.json`)
-    console.log(response.data)
+    const response = await axiosApiInstance.get(url)
     cards.value = response.data
   } catch (err) {
     console.log(err.response)
