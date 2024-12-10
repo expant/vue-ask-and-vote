40d589887ff73c008dc99f76397f3de9e31499d8
Expant <anton532202@gmail.com>
2024-12-10 16:59:43 +0300

    layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,30 @@
 <script setup>
 import QuestionCards from '@/components/QuestionCards.vue'
+import { useAuthStore } from '@/stores/auth'
+import LogOut from '@/components/LogOut.vue'
+
+const authStore = useAuthStore()
 </script>
 
 <template>
   <div class="container my-0 mx-auto font-IBMPlexMono">
     <header class="flex justify-around items-center py-10">
       <h1 class="font-bold text-3xl text-green-500">Ask & Vote</h1>
-      <div>
-        <button class="font-medium text-xl text-gray-600 hover:text-gray-700">Профиль</button>
+      <div v-if="!authStore.userInfo.token" class="flex items-center gap-5">
+        <router-link to="/signin" class="font-medium text-xl text-gray-600 hover:text-gray-700"
+          >Войти</router-link
+        >
+        <router-link
+          to="/signup"
+          class="transition font-medium text-xl text-green-600 px-3 py-2 border border-solid border-green-600 rounded-md hover:bg-green-600 hover:text-white"
+          >Зарегистрироваться</router-link
+        >
+      </div>
+      <div v-else class="flex items-center gap-5">
+        <router-link to="/profile" class="font-medium text-xl text-gray-600 hover:text-gray-700"
+          >Профиль</router-link
+        >
+        <LogOut />
       </div>
     </header>
     <main class="flex justify-center">
