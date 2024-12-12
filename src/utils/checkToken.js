d189c21a4f951b0b49ad8a3588ca4d370d6c8da2
Expant <anton532202@gmail.com>
2024-12-12 17:09:52 +0300

    update user data storage

--- src/utils/checkToken.js
+++
@@ -1,7 +0,0 @@
-export default (authStore) => {
-  const tokens = JSON.parse(localStorage.getItem('userTokens'))
-  if (tokens) {
-    authStore.userInfo.token = tokens.token
-    authStore.userInfo.refreshToken = tokens.refreshToken
-  }
-}
