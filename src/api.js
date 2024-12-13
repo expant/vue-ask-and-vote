9ce1614683c3cd5c30e93126e4e9538ee434fcf8
Expant <anton532202@gmail.com>
2024-12-13 15:46:19 +0300

    refactor

--- src/api.js
+++ src/api.js
@@ -5,8 +5,9 @@ import router from './router'
 const axiosApiInstance = axios.create()
 const apiKey = import.meta.env.VITE_API_KEY_FIREBASE
 
-axiosApiInstance.interceptors.request.use((config) => {
+const insertTokenIntoUrl = (config) => {
   const url = config.url
+
   if (url.includes('signInWithPassword') || url.includes('signUp')) {
     return config
   }
@@ -16,44 +17,45 @@ axiosApiInstance.interceptors.request.use((config) => {
   params.append('auth', authStore.userInfo.token)
   config.params = params
   return config
-})
+}
 
-axiosApiInstance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const authStore = useAuthStore()
-    const originalRequest = error.config
+const refreshTokenByError = async (error) => {
+  const authStore = useAuthStore()
+  const originalRequest = error.config
 
-    if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true
+  if (error.response.status !== 401 || originalRequest._retry) {
+    return Promise.reject(error)
+  }
 
-      try {
-        const refresh_token = JSON.parse(localStorage.getItem('userTokens')).refreshToken
-        const newTokens = await axios.post(
-          `https://securetoken.googleapis.com/v1/token?key=${apiKey}`,
-          {
-            grant_type: 'refresh_token',
-            refresh_token,
-          },
-        )
-        authStore.userInfo.token = newTokens.data.access_token
-        authStore.userInfo.refreshToken = newTokens.data.refresh_token
-        localStorage.setItem(
-          'userTokens',
-          JSON.stringify({
-            token: newTokens.data.access_token,
-            refreshToken: newTokens.data.refresh_token,
-          }),
-        )
-      } catch (err) {
-        localStorage.removeItem('userTokens')
-        router.push('/signin')
-        authStore.userInfo.token = ''
-        authStore.userInfo.refreshToken = ''
-      }
-    }
+  originalRequest._retry = true
 
-    console.log(error)
-  },
-)
+  try {
+    const refresh_token = JSON.parse(localStorage.getItem('userTokens')).refreshToken
+    const newTokens = await axios.post(
+      `https://securetoken.googleapis.com/v1/token?key=${apiKey}`,
+      {
+        grant_type: 'refresh_token',
+        refresh_token,
+      },
+    )
+    authStore.userInfo.token = newTokens.data.access_token
+    authStore.userInfo.refreshToken = newTokens.data.refresh_token
+    localStorage.setItem(
+      'userTokens',
+      JSON.stringify({
+        token: newTokens.data.access_token,
+        refreshToken: newTokens.data.refresh_token,
+      }),
+    )
+  } catch (err) {
+    localStorage.removeItem('userTokens')
+    router.push('/signin')
+    authStore.userInfo.token = ''
+    authStore.userInfo.refreshToken = ''
+  }
+}
+
+axiosApiInstance.interceptors.request.use(insertTokenIntoUrl)
+axiosApiInstance.interceptors.response.use((response) => response, refreshTokenByError)
+
 export default axiosApiInstance
