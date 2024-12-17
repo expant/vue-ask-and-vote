7916bbcfd92c914475d68b8df60a66926acfafe2
Expant <anton532202@gmail.com>
2024-12-17 16:56:31 +0300

    work with cards

--- src/components/QuestionCards.vue
+++ src/components/QuestionCards.vue
@@ -3,12 +3,9 @@ import axiosApiInstance from '@/api'
 import QuestionCardsItem from '@/components/QuestionCardsItem.vue'
 import ClipLoader from 'vue-spinner/src/ClipLoader.vue'
 import { useAuthStore } from '@/stores/auth'
-import { ref, onMounted } from 'vue'
-// import { useRouter } from 'vue-router'
+import { ref, onMounted, watch } from 'vue'
 
 const authStore = useAuthStore()
-// const routes = useRouter()
-
 const cards = ref([])
 const showLoader = ref(false)
 const databaseUrl = import.meta.env.VITE_FIREBASE_DB_QUESTIONS_URL + '.json'
@@ -17,14 +14,18 @@ const getAllCards = async () => {
   showLoader.value = true
   try {
     const response = await axiosApiInstance.get(databaseUrl)
-    cards.value = response.data.map((card) => ({
+    const data = Object.entries(response.data)
+
+    cards.value = data.map(([id, card]) => ({
       text: card.text,
       options: card.options,
       createdAt: card.createdAt,
       totalVotes: card.totalVotes || 0,
+      voterIds: card.voterIds || [],
+      id,
     }))
   } catch (err) {
-    console.log(err.response)
+    console.log(err)
   } finally {
     showLoader.value = false
   }
@@ -33,19 +34,22 @@ const getAllCards = async () => {
 onMounted(async () => {
   await getAllCards()
 })
+
+watch(authStore.userInfo, getAllCards())
 </script>
 
 <template>
-  <ul class="flex flex-col gap-7">
+  <ul class="w-full flex flex-col gap-7 md:w-1/3">
     <ClipLoader v-if="showLoader" />
     <QuestionCardsItem
       v-for="(card, i) in cards"
       :key="i"
-      :id="i"
+      :id="card.id"
       :text="card.text"
       :options="card.options"
       :createdAt="card.createdAt"
       :totalVotes="card.totalVotes"
+      :voterIds="card.voterIds"
     />
   </ul>
 </template>
--- src/components/QuestionCardsItem.vue
+++ src/components/QuestionCardsItem.vue
@@ -1,11 +1,14 @@
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import axiosApiInstance from '@/api'
 import PersonIcon from './icons/PersonIcon.vue'
+import { useAuthStore } from '@/stores/auth'
+
+const authStore = useAuthStore()
 
 const props = defineProps({
   id: {
-    type: Number,
+    type: String,
     required: true,
   },
   text: {
@@ -20,33 +23,68 @@ const props = defineProps({
     type: String,
     required: true,
   },
-  // voted: {
-  //   type: Boolean,
-  //   required: true,
-  // },
   totalVotes: {
     type: Number,
     required: true,
   },
+  voterIds: {
+    type: Array,
+    required: true,
+  },
 })
 
+const card = ref({})
 const picked = ref('')
+const voted = ref(false)
 
-const vote = async () => {
-  const pickedIndex = props.options.map((item) => item.title).indexOf(picked.value)
-  const urlToQuestion = `${import.meta.env.VITE_FIREBASE_DB_QUESTIONS_URL}/${props.id}/.json`
-  props.options[pickedIndex].votersCount = props.options[pickedIndex].votersCount + 1
-  const data = {
-    totalVotes: props.totalVotes + 1,
-    options: props.options,
+const fetchVotersIds = async () => {
+  const urlToVoterIds = `${import.meta.env.VITE_FIREBASE_DB_QUESTIONS_URL}/${card.value.id}/voterIds/.json`
+  const responseVoterIds = await axiosApiInstance.get(urlToVoterIds)
+
+  if (!responseVoterIds.data) {
+    return
   }
 
-  console.log(data)
+  const userId = responseVoterIds.data.find((id) => id === authStore.userInfo.userId)
+
+  console.log(responseVoterIds.data, authStore.userInfo.userId)
+  if (userId) {
+    voted.value = true
+  }
+}
+
+onMounted(() => {
+  card.value = {
+    id: props.id,
+    text: props.text,
+    options: props.options,
+    createdAt: props.createdAt,
+    totalVotes: props.totalVotes,
+    voterIds: props.voterIds,
+  }
+
+  fetchVotersIds()
+})
+
+const vote = async () => {
+  const pickedIndex = card.value.options.map((item) => item.text).indexOf(picked.value)
+  const urlToQuestion = `${import.meta.env.VITE_FIREBASE_DB_QUESTIONS_URL}/${card.value.id}/.json`
+  card.value.options[pickedIndex].votersCount = card.value.options[pickedIndex].votersCount + 1
+  card.value.totalVotes = card.value.totalVotes + 1
+  card.value.voterIds.push(authStore.userInfo.userId)
+
+  const data = {
+    totalVotes: card.value.totalVotes,
+    options: card.value.options,
+    voterIds: card.value.voterIds,
+  }
 
   try {
-    const response1 = await axiosApiInstance.patch(urlToQuestion, JSON.stringify(data))
+    fetchVotersIds()
 
-    console.log(response1)
+    const response = await axiosApiInstance.patch(urlToQuestion, JSON.stringify(data))
+
+    voted.value = true
   } catch (err) {
     console.log(err)
   }
@@ -54,11 +92,11 @@ const vote = async () => {
 </script>
 
 <template>
-  <li class="p-6 border border-solid border-gray-200 rounded-lg shadow-sm">
-    <h3 class="font-bold text-green-600 mb-5">{{ props.text }}</h3>
+  <li v-if="!voted" class="p-6 border border-solid border-gray-200 rounded-lg shadow-sm">
+    <h3 class="font-bold text-green-600 mb-5">{{ card.text }}</h3>
     <div class="flex flex-col gap-2.5 w-full">
       <div
-        v-for="(option, i) in props.options"
+        v-for="(option, i) in card.options"
         :key="i"
         class="transition text-left text-sm p-2 bg-slate-100 rounded hover:bg-slate-200"
       >
@@ -66,36 +104,37 @@ const vote = async () => {
           @change="vote"
           type="radio"
           :id="`option-${i}`"
-          :value="option.title"
+          :value="option.text"
           v-model="picked"
         />
-        <label :for="`option-${i}`" class="ml-2">{{ option.title }}</label>
+        <label :for="`option-${i}`" class="ml-2">{{ option.text }}</label>
       </div>
     </div>
     <div class="flex justify-end gap-1 text-xs text-gray-400 text-right mt-3">
-      <span>{{ props.totalVotes }}</span>
+      <span>{{ card.totalVotes }}</span>
       <PersonIcon size="14px" color="#9CA3AF" />
     </div>
   </li>
-  <!-- <li
-    v-if="props.voted"
-    class="p-10 bg-gray-200 border border-solid border-gray-20 rounded-lg shadow-sm"
-  >
-    <h3 class="font-bold text-green-600 mb-5">{{ props.text }}</h3>
+
+  <li v-if="voted" class="p-10 bg-gray-200 border border-solid border-gray-20 rounded-lg shadow-sm">
+    <h3 class="font-bold text-green-600 mb-5">{{ card.text }}</h3>
     <div class="flex flex-col gap-2.5 w-full">
       <button
-        v-for="(option, i) in props.options"
+        v-for="(option, i) in card.options"
         :key="i"
-        class="flex justify-between transition text-left text-sm p-2 bg-slate-300 rounded"
+        :class="[
+          'flex justify-between transition text-left text-sm p-2 rounded',
+          option.text === picked ? 'bg-slate-400' : 'bg-slate-300',
+        ]"
         disabled="disabled"
       >
-        <span>{{ option.title }}</span>
+        <span>{{ option.text }}</span>
         <span class="text-gray-500">{{ option.votersCount }}</span>
       </button>
     </div>
     <div class="flex justify-end gap-1 text-xs text-gray-400 text-right mt-3">
-      <span class="">{{ props.totalVotes }}</span>
+      <span class="">{{ card.totalVotes }}</span>
       <PersonIcon size="14px" color="#9CA3AF" />
     </div>
-  </li> -->
+  </li>
 </template>
