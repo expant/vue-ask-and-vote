f8a0130a1abd4bc6ca6d861713438cf3e11e269b
Expant <anton532202@gmail.com>
2024-12-19 16:56:41 +0300

    get cards again after logout :bug:

--- src/components/LogOut.vue
+++ src/components/LogOut.vue
@@ -1,12 +1,21 @@
 <script setup>
 import LogOutIconItem from './icons/LogOutIconItem.vue'
 import { useAuthStore } from '@/stores/auth'
+import { useCardsStore } from '@/stores/cards'
+import { useRouter } from 'vue-router'
 
 const authStore = useAuthStore()
+const cardsStore = useCardsStore()
+const router = useRouter()
 
 const logout = () => {
+  const currentViewPath = router.currentRoute.value.path
   authStore.logout()
   localStorage.removeItem('userData')
+
+  if (currentViewPath === '/') {
+    cardsStore.getAllCards()
+  }
 }
 </script>
 
